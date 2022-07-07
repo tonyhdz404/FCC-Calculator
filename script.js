59846175dba8051e2151f7eca06113260af3aba6
tonyhdz404 <antonio.hernandez030@gmail.com>
2022-07-07 12:48:31 -0600

    16/17 Tests passed

--- script.js
+++ script.js
@@ -5,8 +5,8 @@ const output = document.querySelector(".display__output");
 const operatorValues = {
   X: 4,
   "/": 3,
-  "+": 2,
-  "-": 0,
+  "+": 0,
+  "-": 2,
 };
 function operatorSort(a, b) {
   if (operatorValues[a] > operatorValues[b]) {
@@ -19,15 +19,43 @@ function operatorSort(a, b) {
 }
 
 function calculate() {
-  const expression = output.innerText.split(/(\D)/);
+  //* 1) Creating an array that is split AT any operator
+  const rawExpression = output.innerText.split(/([-+\/X])/);
+
+  //* 2) Checking if only ONE number was entered
+  if (rawExpression.length === 1) {
+    return (output.innerText = rawExpression[0].replace(/(?!\d).(?=\.)/g, ""));
+  }
+  //* 3) Formatting the numbers in the raw expression
+  const expression = rawExpression.map((number, index, array) => {
+    if (
+      number.match(/(?!\d).(?=\.)/g) &&
+      number.match(/(?!\d).(?=\.)/g).length >= 1
+    ) {
+      const formattedNumber = number.replace(/(?!\d).(?=\.)/g, "");
+      return formattedNumber;
+    }
+    if (/\d/.test(number)) {
+      if (index > 0 && array[index - 1] === "-" && array[index - 2] === "") {
+        const negativeNumber = 0 - +number;
+        console.log(rawExpression.splice(index - 2, 3, negativeNumber));
+        console.log(rawExpression);
+        return;
+      }
+      return +number;
+    }
+    return number;
+  });
+  console.log(expression);
+
+  //* 4) creating an array of all the operators and sorting them
   const operators = output.innerText.match(/[-+\/X]/g);
-  let result;
   operators.sort((a, b) => operatorSort(a, b));
 
+  //* 5) Looping through all the operators
+  let result;
   for (let i = 0; i < operators.length; i++) {
-    console.log(`Start`, expression);
     const operator = operators[i];
-    console.log(operator);
     const operatorIndex = expression.indexOf(operator);
 
     switch (operator) {
@@ -52,7 +80,6 @@ function calculate() {
         break;
     }
     expression.splice(operatorIndex - 1, 3, result);
-    console.log(`End`, expression);
   }
 
   output.innerText = result;
@@ -70,6 +97,16 @@ calculator.addEventListener("click", function (e) {
   const buttonValue = button.innerText;
   if (output.innerText === "0" && !button.classList.contains("operator")) {
     return (output.innerText = buttonValue);
+  }
+
+  //* Decimal Validation
+  if (button.id === "decimal") {
+    if (!/([-+\/X])/.test(output.innerText) && /\./.test(output.innerText))
+      return;
+    if (/([-+\/X])/.test(output.innerText)) {
+      const lastNumber = output.innerText.split(/([-+\/X])/).at(-1);
+      if (/\./.test(lastNumber)) return;
+    }
   }
   output.innerText += buttonValue;
 });
